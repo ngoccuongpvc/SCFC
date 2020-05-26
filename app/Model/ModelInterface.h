c6f296cec8a5b4bf404a2166c2f0b212d516d5de
ngoccuongpvc <ngoccuongpvc@gmail.com>
2020-05-26 21:21:40 +0700

    implement our own vector

--- app/Model/ModelInterface.h
+++ app/Model/ModelInterface.h
@@ -4,7 +4,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <vector>
+#include "../Utils/vector.h"
 #include <map>
 #include <functional>
 #include <cmath>
--- app/View/View.h
+++ app/View/View.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include "../Utils/vector.h"
 #include <iomanip>
 #include <fstream>
 
