29f2d616da51e756ec1f5c6173cc7b3f889a3823
FinisTP <53522152+FinisTP@users.noreply.github.com>
2020-05-28 11:20:14 +0700

    proj file

--- app/Controller/Validation.h
+++ app/Controller/Validation.h
@@ -62,7 +62,7 @@ public:
 		regList["score"] = regex("\\d+");
 
 		regList["gender"] = regex("male|female");
-		regList["term"] = regex("mid|final|bonus|lab");
+		regList["term"] = regex("midterm|final|bonus|lab");
 		regList["sem"] = regex("spring|summer|fall|winter");
 	}
 
